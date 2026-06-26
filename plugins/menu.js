224b69f1ca730c70674c35e4d6e56278fc4dc33f
Hiura <hiura0012@gmail.com>
2026-06-27 00:18:01 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,25 +1,23 @@
 async function menu(sock, sender, message) {
-
   const templates = `
-╭❰  *✯ RESBOT JPM ✯*  ❱
-│
-│ Status : ACTIVE
-│ User   : ${sender.split('@')[0]}
-╰────────────❱
+ㅤ  ㅤ       *(ㅤ  ㅤ  ${global.botName} ㅤ  ㅤ )* 
+ㅤ  𝗌𝗍𝖺𝗍𝗎𝗌  𝖺𝖼𝗍𝗂𝗏𝖾  •  𝗎𝗌𝖾𝗋  ${sender.split('@')[0]}
+ㅤ  𝟢𝟢  .. 🎱  *𝖿𝗈𝗋𝗆𝖺𝗍  " \`𝗮𝗹𝗹 𝗶𝗻 𝗼𝗻𝗲\`  " /*  ¿?  /
 
-┌─ *COMMANDS*
-│
-│ ➤  ʟɪꜱᴛɢᴄ
-│ ➤  ᴀᴜᴛᴏᴊᴘᴍ
-│ ➤  ᴀᴜᴛᴏʀᴇᴘʟʏ
-│ ➤  ᴊᴘᴍ
-│ ➤  ᴊᴘᴍᴛᴀɢ
-│ ➤  ᴘᴜꜱʜᴋᴏɴᴛᴀᴋ
-└───────────
+ㅤ   🕸   •   🕷─── *𝖺  𝗆𝗈𝗈𝗇𝗅𝗂𝗀𝗁𝗍  ╮*  
+ㅤ  *𝗆𝖾𝗇𝗎   𝗅𝗂𝗌𝗍   𝖺𝗏𝖺𝗂𝗅𝖺𝖻𝗅𝖾   𝗇𝗈𝗐 - !* 
+ㅤ  │┊  ☍ .  𝗅𝗂𝗌𝗍𝗀𝖼
+ㅤ  │┊  ☍ .  𝖺𝗎𝗍𝗈𝗃𝗉𝗆
+ㅤ  │┊  ☍ .  𝖺𝗎𝗍𝗈𝗋𝖾𝗉𝗅𝗒
+ㅤ  │┊  ☍ .  𝗃𝗉𝗆
+ㅤ  │┊  ☍ .  𝗃𝗉𝗆𝗍𝖺𝗀
+ㅤ  │┊  ☍ .  𝗉𝗎𝗌𝗁𝗄𝗈𝗇𝗍𝖺𝗄
+ㅤ  ╰─────────────────────`;
 
-© autoresbot.com`;
-
-  await sock.sendMessage(sender, { text: templates });
+  await sock.sendMessage(sender, {
+    image: { url: global.banner },
+    caption: templates,
+  });
 }
 
 export default menu;
